1b0b8257fe228b938bcf74219940acabd3ff36ec
Alicia Quezada <aqueza2@gmail.com>
2015-10-24 22:37:25 -0500

    add good guess answers

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,22 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: Malia Lehrer ].
+
+
+# Your Solution Below
+def good_guess?(number)
+  if number == 42
+    p true
+  else
+    p false
+  end
+end
+
+good_guess?(40)
+
+#Pseudocode
+#Input: integer
+#Output: boolean
+#if 42, true
+#if not 42, false
+
