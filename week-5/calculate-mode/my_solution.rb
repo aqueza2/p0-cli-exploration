0d21adc459db63cc3c46d50c34226c71e831cdff
Alicia Quezada <aqueza2@gmail.com>
2015-11-02 01:07:28 -0600

    add 5.3 reflection

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -19,14 +19,13 @@
 # 3- Check which values match
 # 4- Put those values into new array
 # 5- Returns an array with the most frequent values.
+
 # 1. Initial Solution
 # def mode array
 # hash = Hash.new(0)
 # array.each do |x|
 # hash[x] += 1
 # end
-
-
 # new_array = Array.new
 # var1 = 0
 # hash.each do |key, value|
@@ -34,11 +33,12 @@
 # give.push (key)
 # else value > var1
 # give = [key]
-
 # end
 # end
 # return new_array
 # end
+
+
 # 3. Refactored Solution
 def mode(array)
   frequency = Hash.new(0)
@@ -56,3 +56,13 @@ end
 
 
 # 4. Reflection
+=begin
+Which data structure did you and your pair decide to implement and why?
+We used hashes and arrays. Mostly our solution contains hashes.
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+Yes, we were able to break it down as much as we could, but the pseudocode was not very helpful for this challenge. This challenge was difficult.
+What issues/successes did you run into when translating your pseudocode to code?
+We had to look up a lot of things because our initial solution was not working. Our pseudocode was not as detailed as it should have been.
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+We used .each do to iterate through the array and throught the hash that we used. We didn't use any new methods that we didn't already know.
+=end
