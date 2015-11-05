86c9cabc4d5f41940dd0f60661fe9cdd9776ce23
Alicia Quezada <aqueza2@gmail.com>
2015-11-04 22:47:44 -0600

    add guessing game file

---
+++ week-6/guessing-game/my_solution.rb
@@ -0,0 +1,38 @@
+# Build a simple guessing game
+
+
+# I worked on this challenge [by myself].
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# Initial Solution
+
+class GuessingGame
+  def initialize(answer)
+    @guess = answer
+  end
+
+  def guess
+    @guess
+  end
+
+  def solved
+    @guess
+  end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# Reflection
