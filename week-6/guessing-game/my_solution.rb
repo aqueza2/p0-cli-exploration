e5f81bb02d99fd4ef7dbd6474e4fd5e133e227b1
Alicia Quezada <aqueza2@gmail.com>
2015-11-05 22:10:14 -0600

    add solution to file

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge [by myself].
-# I spent [#] hours on this challenge.
+# I spent [1] hours on this challenge.
 
 # Pseudocode
 
@@ -12,25 +12,51 @@
 
 
 # Initial Solution
+# class GuessingGame
+#   def initialize(answer)
+#     @answer = answer
+#   end
 
-class GuessingGame
-  def initialize(answer)
-    @guess = answer
-  end
+#   def guess(guess)
+#     @guess = guess
+#     return :high if @guess > @answer
+#     return :correct if @guess == @answer
+#     return :low if @guess < @answer
+#   end
 
-  def guess
-    @guess
-  end
-
-  def solved
-    @guess
-  end
+#   def solved?
+#     if @guess == @answer
+#       true
+#     else
+#       false
+#     end
+#   end
+# end
 
 
 
 # Refactored Solution
 
+class GuessingGame
+  def initialize(answer)
+    @answer = answer
+  end
 
+  def guess(guess)
+    @guess = guess
+    if @guess > @answer
+      :high
+    elsif @guess == @answer
+      :correct
+    else @guess < @answer
+      :low
+    end
+  end
+
+  def solved?
+    @guess == @answer
+  end
+end
 
 
 
