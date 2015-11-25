b5ae6c3ef04c7bc3b5ea2d0767163001e04e905f
Alicia Quezada <aqueza2@gmail.com>
2015-11-24 20:35:41 -0600

    start

--- week-9/dom-manipulation/home-page.js
+++ week-9/dom-manipulation/home-page.js
@@ -1,19 +1,28 @@
 // DOM Manipulation Challenge
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge [by myself, with: Ryan Zell].
 
 
 // Add your JavaScript calls to this page:
 
 // Release 0:
+var release0 = document.getElementById("release-0");
+id = "release-0"
+var att = document.createAttribute("class");
+att.value = "done";
+release0.setAttributeNode(att)
 
 
 
 
 // Release 1:
 
-
+var release0 = document.getElementById("release-1");
+id = "release-1"
+var att = document.createAttribute("class");
+att.value = "none";
+release0.setAttributeNode(att)
 
 
 // Release 2:
